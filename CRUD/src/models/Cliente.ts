d0fc68dd79ece123286538a6e48fa8311eb98a59
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-10 18:29:36 -0600

    Feat: Se agrego el model de ClienteEnums

---
+++ CRUD/src/models/Cliente.ts
@@ -0,0 +1,10 @@
+export enum RangoCliente{
+    Regular = "Regular",
+    Vip = "Vi",
+    Mayoritario = "Mayoritario"
+}
+
+export enum EstadoCliente{
+    Activo = "Activo",
+    Inactivo = "Inactivo"
+}
---
+++ CRUD/src/models/ClienteEnums.ts
@@ -0,0 +1,10 @@
+export enum RangoCliente{
+    Regular = "Regular",
+    Vip = "Vip",
+    Mayoritario = "Mayoritario"
+}
+
+export enum EstadoCliente{
+    Activo = "Activo",
+    Inactivo = "Inactivo"
+}
