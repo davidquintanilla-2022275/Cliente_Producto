d20644e3c0f0c45184eca82d989c4875b5aa173b
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-10 18:56:33 -0600

    Feat: Se agrego el model de ProductoEnums

---
+++ CRUD/src/models/ProductoEnums.ts
@@ -0,0 +1,13 @@
+export enum Categoria{
+    Ropa = "Ropa",
+    Limpieza = "Limpieza",
+    Electrodomesticos = "Electrodomesticos",
+    Tecnologia = "tecnologia",
+    Muebles = "Muebles"
+}
+
+export enum Estado{
+    Venta = "venta",
+    Descontinuado = "Descontinuado",
+    Borrador = "Borrador"
+}
