ea3c0163aca78fd5e584c3d83ec10d1cfbca1ebf
David Enrique Quintanilla Pérez <dquintanilla-2022275@kinal.edu.gt>
2026-07-10 18:38:07 -0600

    Feat: se agrego el model de Cliente

--- CRUD/src/models/Cliente.ts
+++ CRUD/src/models/Cliente.ts
@@ -1,10 +1,12 @@
-export enum RangoCliente{
-    Regular = "Regular",
-    Vip = "Vi",
-    Mayoritario = "Mayoritario"
-}
+import {RangoCliente, EstadoCliente} from "./ClienteEnums"
 
-export enum EstadoCliente{
-    Activo = "Activo",
-    Inactivo = "Inactivo"
+export interface Cliente{
+    id: number,
+    nombre: string,
+    apellido: string,
+    telefono: number,
+    edad: number,
+    dpi: number,
+    rango: RangoCliente,
+    estado: EstadoCliente
 }
